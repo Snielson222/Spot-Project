6595fdfcfdb016f0de673d87c4fbeef4a94c2b37
Steven Nielson <snielson222@gmail.com>
2023-09-06 12:45:44 -0400

    finished get all spots endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,9 +5,16 @@ const { ResultWithContextImpl } = require('express-validator/src/chain');
 
 const router = express.Router()
 
+
+
 router.get('/', async (req, res, next) => {
 
-const spots = await Spot.findAll()
+const spots = await Spot.findAll({
+    include: [
+        {model: Review},
+        {model: SpotImage}
+    ]
+})
 
 if (!spots) {
     res.status(404)
@@ -15,19 +22,31 @@ if (!spots) {
         "message": "Spot couldn't be found"
     })
 }
+let spotsList = [];
+spots.forEach(element => {
+    spotsList.push(element.toJSON());
 
-// for (let i = 0; i < spots.length; i++) {
-// const avgRating = await Review.findAll({
-//     attributes: ['stars']
-// })
-// const previewImg = await SpotImage.findAll({
-//     attributes: ['url']
-// })
-// spots.dataValues.avgRating = avgRating;
-// spots.dataValues.previewImg = previewImg;
-// }
+});
 
-return res.json(spots)
+spotsList.forEach(spot => {
+    spot.Reviews.forEach(review => {
+        let sum = 0
+        sum += review.stars
+        let avg = sum/spot.Reviews.length
+        spot.avgRating = avg
+    })
+    delete spot.Reviews
+})
+
+spotsList.forEach(spot => {
+    spot.SpotImages.forEach(image => {
+        spot.previewImage = image.url
+        if (!spot.previewImage) spot.previewImage = "No Image Found"
+    })
+    delete spot.SpotImages
+})
+
+return res.json({"Spots":spotsList})
 
 });
 
