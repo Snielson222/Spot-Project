3ef8d76d5a7f30f50a287aaec7109c1c05401096
Steven Nielson <snielson222@gmail.com>
2023-09-06 17:16:59 -0400

    finished get all current user spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const { requireAuth } = require("../../utils/auth");
 
 //GET SPOT BY ID !!!WORKING!!!
-router.get("/:spotId", async (req, res, next) => {
+router.get("/:spotId(\\d+)", async (req, res, next) => {
   let spots = await Spot.findByPk(req.params.spotId, {
     include: [
       { model: SpotImage, attributes: ["id", "url", "preview"] },
@@ -42,6 +42,29 @@ router.get("/:spotId", async (req, res, next) => {
   delete spots.User;
 
   return res.json(spots);
+});
+router.post('/', requireAuth, async (req, res, next) => {
+    const {address, city, state, country, lat, lng, name, description, price} = req.body;
+    
+    if (!req.body) {
+        res.status(400)
+        return res.json({
+            "message": "Bad Request",
+  "errors": {
+    "address": "Street address is required",
+    "city": "City is required",
+    "state": "State is required",
+    "country": "Country is required",
+    "lat": "Latitude is not valid",
+    "lng": "Longitude is not valid",
+    "name": "Name must be less than 50 characters",
+    "description": "Description is required",
+    "price": "Price per day is required"
+  }
+        })
+    };
+    const newSpot = Spot.build
+
 });
 //GET ALL SPOTS !!!WORKING!!!
 router.get("/", async (req, res, next) => {
@@ -81,13 +104,13 @@ router.get("/", async (req, res, next) => {
   return res.json({ Spots: spotsList });
 });
 
-//GET ALL SPOTS OWNED BY CURRENT USER
+//GET ALL SPOTS OWNED BY CURRENT USER !!!WORKING!!!
 router.get("/current", requireAuth, async (req, res, next) => {
-    console.log(req.user, '********')
-    const spots = await User.findAll({
-        where: { "ownerId": req.user.id },
-        include: [{ model: Review }, { model: SpotImage }],
+    const spots = await Spot.findAll({
+        where: { "ownerId": req.user.dataValues.id },
+        include: [{ model: Review }, {model: User}, {model: SpotImage}],
     });
+  
     if (!spots) {
         res.status(404);
         return res.json({
@@ -95,6 +118,30 @@ router.get("/current", requireAuth, async (req, res, next) => {
         });
     };
 
-    return res.json(spots)
+    let spotsList = [];
+    spots.forEach((element) => {
+      spotsList.push(element.toJSON());
+    });
+  
+    spotsList.forEach((spot) => {
+      spot.Reviews.forEach((review) => {
+        let sum = 0;
+        sum += review.stars;
+        let avg = sum / spot.Reviews.length;
+        spot.avgRating = avg;
+      });
+      delete spot.Reviews;
+    });
+  
+    spotsList.forEach((spot) => {
+      spot.SpotImages.forEach((image) => {
+        spot.previewImage = image.url;
+        if (!spot.previewImage) spot.previewImage = "No Image Found";
+      });
+      delete spot.SpotImages;
+      delete spot.User;
+    });
+  
+    return res.json({ Spots: spotsList })
 });
 module.exports = router;
