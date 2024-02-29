d160355733fefb82d6e8c44900ddbc40dae6d8fb
Steven Nielson <snielson222@gmail.com>
2024-02-29 12:28:51 -0500

    functionality of search bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import imgSrc from '../../images/FullLogo_Transparent.png'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import imgSrc from "../../images/FullLogo_Transparent.png";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  function submitForm() {
+
+  }
 
   return (
-    
-    <div className='navContainer'>
-      <div className='navBar'>
-      <div>
-        <NavLink className="home" exact to="/"><img className="logo" alt="logo" src={imgSrc}></img></NavLink>
+    <div className="navContainer">
+      <div className="navBar">
+        <div>
+          <NavLink className="home" exact to="/">
+            <img className="logo" alt="logo" src={imgSrc}></img>
+          </NavLink>
+        </div>
+        <div className="createAndProfileContainer">
+          <div>
+            <NavLink className="create" exact to="/spots/new">
+              {sessionUser ? "Create a New Spot" : ""}
+            </NavLink>
+          </div>
+          {isLoaded && (
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          )}
+        </div>
       </div>
-      <div className='createAndProfileContainer'>
-
-      <div>
-        <NavLink className="create" exact to='/spots/new'>{sessionUser ? "Create a New Spot" : ""}</NavLink>
+      <div className="searchNav">
+        <label className="searchLabel">Where
+        <form>
+          <input placeholder='Search destinations' className="searchInput"></input>
+          <button id="searchButton" type="submit" onClick={submitForm}><i class="fa fa-search" aria-hidden="true"></i> Search</button>
+        </form>
+        </label>
       </div>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-      </div>
-    </div>
-    <div className='searchNav'>
-      <label className='searchLabel'>
-      <input className='searchInput'></input>
-      </label>
-    </div>
     </div>
   );
 }
