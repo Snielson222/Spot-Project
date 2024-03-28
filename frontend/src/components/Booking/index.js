750c06ecfe1c2e9118e22801a65924cdd2298d0c
Steven Nielson <snielson222@gmail.com>
2024-03-28 12:00:28 -0400

    fleshing out booing componenet to work with API and REDUX store

--- frontend/src/components/Booking/index.js
+++ frontend/src/components/Booking/index.js
@@ -1,7 +1,14 @@
-
+import { thunkLoadBookings } from "../../store/booking";
+import { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Booking = () => {
 
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(thunkLoadBookings())
+    }, [dispatch])
 
     return (<div>
         BOOKING!!!!
--- frontend/src/store/booking.js
+++ frontend/src/store/booking.js
@@ -1,108 +1,109 @@
-import {csrfFetch} from './csrf'
+import { csrfFetch } from "./csrf";
 
-export const LOAD_BOOKINGS = 'bookings/LOAD_BOOKINGS'
+export const LOAD_BOOKINGS = "bookings/LOAD_BOOKINGS";
 
-export const CREATE_BOOKING = 'bookings/CREATE_BOOKING'
+export const CREATE_BOOKING = "bookings/CREATE_BOOKING";
 
-export const DELETE_BOOKING = 'bookings/DELETE_BOOKING'
+export const DELETE_BOOKING = "bookings/DELETE_BOOKING";
 
-export const EDIT_BOOKING = 'bookings/EDIT_BOOKING'
+export const EDIT_BOOKING = "bookings/EDIT_BOOKING";
 
 export const loadBookings = (booking) => ({
-    type: LOAD_BOOKINGS,
-    booking
-})
+  type: LOAD_BOOKINGS,
+  booking,
+});
 
 export const createBooking = (booking) => ({
-    type: CREATE_BOOKING,
-    booking
-})
+  type: CREATE_BOOKING,
+  booking,
+});
 
 export const deleteBooking = (booking) => ({
-    type: DELETE_BOOKING,
-    booking
-})
+  type: DELETE_BOOKING,
+  booking,
+});
 
 export const editBooking = (booking) => ({
   type: EDIT_BOOKING,
-  booking
-})
+  booking,
+});
 
 export const thunkLoadBookings = (spotId) => async (dispatch) => {
-    const res = await fetch(`/api/bookings/current`)
-    const data = await res.json()
-    dispatch(loadBookings(data))
-    return data
-}
+  const res = await fetch(`/api/bookings/current`);
+  const data = await res.json();
+  dispatch(loadBookings(data));
+  return data;
+};
 
 export const thunkDeleteBooking = (bookingId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/bookings/${bookingId}`, {
-      method: "DELETE"
-    })
-  
-    if (res.status >= 400) {
-      const error = await res.json()
-      return error
-    } else {
-      const data = await res.json()
-      dispatch(deleteBooking(bookingId))
-      return data
-    }
-}
+  const res = await csrfFetch(`/api/bookings/${bookingId}`, {
+    method: "DELETE",
+  });
+
+  if (res.status >= 400) {
+    const error = await res.json();
+    return error;
+  } else {
+    const data = await res.json();
+    dispatch(deleteBooking(bookingId));
+    return data;
+  }
+};
 
 export const thunkCreateBooking = (booking, spotId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/spots/${spotId}/bookings`, {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(booking)
-    })
-    
-    if (!res.ok) {
-      const error = await res.json()
-      return error
-    } else {
-      const data = await res.json()
-      dispatch(thunkLoadBookings(spotId))
-      return data
-    }
-  }
+  const res = await csrfFetch(`/api/spots/${spotId}/bookings`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(booking),
+  });
 
-  export const thunkEditBooking = (booking) => async (dispatch) => {
-    const res = await csrfFetch(`/api/spots/${booking.spot.id}`, {
-      method: "PUT",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(booking)
-    })
-    if (!res.ok) {
-      const error = await res.json()
-      return error
-    } else {
-      const data = await res.json()
-      dispatch(editBooking(data))
-      return data
-    }
+  if (!res.ok) {
+    const error = await res.json();
+    return error;
+  } else {
+    const data = await res.json();
+    dispatch(thunkLoadBookings(spotId));
+    return data;
   }
+};
 
-  const bookingsReducer = (state = {}, action) => {
-    let newState
-    switch (action.type) {
-      case LOAD_BOOKINGS:
-        const bookingState = {};
-        action.booking.Bookings.forEach((booking) => {
-          bookingState[booking.id] = booking;
-        });
-        return bookingState;
-        case DELETE_BOOKING:
-          newState = { ...state };
-          delete newState[action.booking];
-          return newState;
-        case EDIT_BOOKING:
-            newState = {...state}
-            newState[action.booking.id] = action.booking
-            return newState
-        default:
-        return state;
-    }
-  };
-  
-  export default bookingsReducer;
+export const thunkEditBooking = (booking) => async (dispatch) => {
+  const res = await csrfFetch(`/api/spots/${booking.spot.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(booking),
+  });
+  if (!res.ok) {
+    const error = await res.json();
+    return error;
+  } else {
+    const data = await res.json();
+    dispatch(editBooking(data));
+    return data;
+  }
+};
+
+const bookingsReducer = (state = {}, action) => {
+  let newState;
+  switch (action.type) {
+    case LOAD_BOOKINGS:
+      const bookingState = {};
+      action.booking.forEach((booking) => {
+        console.log("🚀 ~ action.booking.forEach ~ action.booking:", action.booking)
+        bookingState[booking.id] = booking;
+      });
+      return bookingState;
+    case DELETE_BOOKING:
+      newState = { ...state };
+      delete newState[action.booking];
+      return newState;
+    case EDIT_BOOKING:
+      newState = { ...state };
+      newState[action.booking.id] = action.booking;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default bookingsReducer;
