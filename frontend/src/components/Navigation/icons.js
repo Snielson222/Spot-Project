25c5ac6a452cda3444d516fbae9a5a3ede25a049
Steven Nielson <snielson222@gmail.com>
2024-05-15 10:05:39 -0400

    fixed styling on nav

--- frontend/src/components/Navigation/icons.js
+++ frontend/src/components/Navigation/icons.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 const iconData = [
-  { icon: <i className="fas fa-bed"></i>, label: 'Bed', tag: 'sleep' },
-  { icon: <i className="fas fa-house-user"></i>, label: 'Home', tag: 'home' },
-  { icon: <i className="fas fa-building"></i>, label: 'Building', tag: 'building' },
-  { icon: <i className="fas fa-car"></i>, label: 'Car', tag: 'car' },
-  { icon: <i className="fas fa-utensils"></i>, label: 'Utensils', tag: 'food' },
-  { icon: <i className="fas fa-swimmer"></i>, label: 'Swimmer', tag: 'swim' },
-  { icon: <i className="fas fa-hiking"></i>, label: 'Hiking', tag: 'outdoor' },
-  { icon: <i className="fas fa-suitcase"></i>, label: 'Suitcase', tag: 'travel' },
-  { icon: <i className="fas fa-dog"></i>, label: 'Dog', tag: 'pet' },
-  { icon: <i className="fas fa-biking"></i>, label: 'Biking', tag: 'outdoor' },
-  // Add more icons and tags as needed
+    { icon: <i className="fas fa-bed"></i>, label: 'Bed', tag: 'sleep' },
+    { icon: <i class="fas fa-home"></i>, label: 'Home', tag: 'home' },
+    { icon: <i className="fas fa-building"></i>, label: 'Building', tag: 'building' },
+    { icon: <i className="fas fa-car"></i>, label: 'Car', tag: 'car' },
+    { icon: <i className="fas fa-utensils"></i>, label: 'Utensils', tag: 'food' },
+    { icon: <i className="fas fa-swimmer"></i>, label: 'Swimmer', tag: 'swim' },
+    { icon: <i className="fas fa-hiking"></i>, label: 'Hiking', tag: 'outdoor' },
+    { icon: <i className="fas fa-suitcase"></i>, label: 'Suitcase', tag: 'travel' },
+    { icon: <i className="fas fa-dog"></i>, label: 'Dog', tag: 'pet' },
+    { icon: <i class="fas fa-bicycle"></i>, label: 'Biking', tag: 'outdoor' },
+    { icon: <i class="fas fa-umbrella-beach"></i>, label: 'Beach', tag: 'beach' },
+    { icon: <i className="fas fa-mountain"></i>, label: 'Mountain', tag: 'mountain' },
+    { icon: <i className="fas fa-camera"></i>, label: 'Photography', tag: 'photography' },
+    { icon: <i className="fas fa-paw"></i>, label: 'Pet Friendly', tag: 'pet-friendly' },
+    { icon: <i className="fas fa-cocktail"></i>, label: 'Cocktails', tag: 'cocktails' },
+    { icon: <i class="fas fa-fish"></i>, label: 'Fishing', tag: 'fishing' },
+    { icon: <i className="fas fa-fire"></i>, label: 'Campfire', tag: 'campfire' },
+    { icon: <i className="fas fa-spa"></i>, label: 'Spa', tag: 'spa' },
+    { icon: <i className="fas fa-golf-ball"></i>, label: 'Golf', tag: 'golf' },
+    { icon: <i className="fas fa-music"></i>, label: 'Music', tag: 'music' },
 ];
+
 
 const Icons = ({ filterSpotsByDescription }) => {
   const handleIconClick = (tag) => {
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,38 +6,30 @@ import "./Navigation.css";
 import imgSrc from "../../images/FullLogo_Transparent.png";
 import { useParams } from "react-router-dom";
 
-
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
-  const param = useParams()
-  console.log("🚀 ~ Navigation ~ param:", param)
-  
+  const param = useParams();
+  console.log("🚀 ~ Navigation ~ param:", param);
 
   return (
     <div className="navContainer">
       <div className="navBar">
-        <div>
-          <NavLink className="home" exact to="/">
-            <img className="logo" alt="logo" src={imgSrc}></img>
-          </NavLink>
-        </div>
+        <NavLink className="home" exact to="/">
+          <img className="logo" alt="logo" src={imgSrc}></img>
+        </NavLink>
         <div className="middleNav">
-          <a href="https://snielson222.github.io/">Portfolio</a>
-          <br></br>
-          <a href="https://github.com/Snielson222">Github</a>
-          <br></br>
-          <a href="https://www.linkedin.com/in/steven-nielson-5b7a3729a/">Linkedin</a>
+          <NavLink to="https://snielson222.github.io/">Portfolio</NavLink>
+          <NavLink to="https://github.com/Snielson222">Github</NavLink>
+          <NavLink to="https://www.linkedin.com/in/steven-nielson-5b7a3729a/">Linkedin</NavLink>
         </div>
         <div className="createAndProfileContainer">
-          <div>
+          {sessionUser && (
             <NavLink className="create" exact to="/spots/new">
-              {sessionUser ? "Create a New Spot" : ""}
+              Create a New Spot
             </NavLink>
-          </div>
+          )}
           {isLoaded && (
-            <li>
-              <ProfileButton user={sessionUser} />
-            </li>
+            <ProfileButton user={sessionUser} />
           )}
         </div>
       </div>
