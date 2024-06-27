e11a3541451f7e2d88e94b269abeeeeabe93702d
Steven Nielson <snielson222@gmail.com>
2024-06-27 09:56:44 -0400

    edited icons for user preference

--- frontend/src/components/Navigation/icons.js
+++ frontend/src/components/Navigation/icons.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const iconData = [
-    { icon: <i className="fas fa-bed"></i>, label: 'Bed', tag: 'sleep' },
-    { icon: <i class="fas fa-home"></i>, label: 'Home', tag: 'home' },
-    { icon: <i className="fas fa-building"></i>, label: 'Building', tag: 'building' },
-    { icon: <i className="fas fa-car"></i>, label: 'Car', tag: 'car' },
+    { icon: <i className="fas fa-bed"></i>, label: 'Quiet', tag: 'sleep' },
+    { icon: <i class="fas fa-home"></i>, label: 'Homey', tag: 'home' },
+    { icon: <i className="fas fa-building"></i>, label: 'Apartment', tag: 'building' },
+    { icon: <i className="fas fa-car"></i>, label: 'Parking', tag: 'car' },
     { icon: <i className="fas fa-utensils"></i>, label: 'Food', tag: 'food' },
     { icon: <i className="fas fa-swimmer"></i>, label: 'Swimming', tag: 'swim' },
     { icon: <i className="fas fa-hiking"></i>, label: 'Hiking', tag: 'outdoor' },
@@ -17,7 +17,7 @@ const iconData = [
     { icon: <i className="fas fa-paw"></i>, label: 'Pet Friendly', tag: 'pet-friendly' },
     { icon: <i className="fas fa-cocktail"></i>, label: 'Cocktails', tag: 'cocktails' },
     { icon: <i class="fas fa-fish"></i>, label: 'Fishing', tag: 'fishing' },
-    { icon: <i className="fas fa-fire"></i>, label: 'Campfire', tag: 'campfire' },
+    { icon: <i className="fas fa-fire"></i>, label: 'Camping', tag: 'campfire' },
     { icon: <i className="fas fa-spa"></i>, label: 'Spa', tag: 'spa' },
     { icon: <i className="fas fa-golf-ball"></i>, label: 'Golf', tag: 'golf' },
     { icon: <i className="fas fa-music"></i>, label: 'Music', tag: 'music' },
